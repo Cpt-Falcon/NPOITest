24ad633835acb23d71edf585d11182ad2e445294
Aubrey Russell <aubrey.russell@hpe.com>
2023-02-01 14:04:08 -0700

    example docs

--- NPOITest/Program.cs
+++ NPOITest/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using NPOI.XWPF.UserModel;
+
+string[] templates = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\TestDocs\\");
+string working = templates.FirstOrDefault(x => Path.GetFileNameWithoutExtension(x) == "WorkingDocExample");
+string linkBreak = templates.FirstOrDefault(x => Path.GetFileNameWithoutExtension(x) == "TestLinkBreak");
+await GenerateNPOIDoc("working", working);
+await GenerateNPOIDoc("TestLinkBreak", linkBreak);
+
+
+async Task GenerateNPOIDoc(string fileName, string inputFile)
+{
+    byte[] bytes = await File.ReadAllBytesAsync(inputFile);
+    using MemoryStream ms = new(bytes);
+    XWPFDocument doc = new(ms);
+
+    MemoryStream result = new();
+    doc.Write(result);
+    result.Flush();
+    result.Position = 0;
+
+    await using FileStream fs = new($"{fileName}.docx", FileMode.Create, FileAccess.Write);
+    fs.Write(result.ToArray());
+}
